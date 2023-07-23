3c43cd3573604cc176af3394ee0935a54d37508e
Noah <noah231515@gmail.com>
2023-07-23 10:46:45 -0400

    browser animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { InputModule } from '@noah231515/receipt-wrangler-core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputModule } from '@noah231515/receipt-wrangler-core';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
