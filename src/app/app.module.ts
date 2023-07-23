9f0de95d24c237cc03fdca144473d8e5604d8f92
Noah <noah231515@gmail.com>
2023-07-23 10:50:51 -0400

    getting ready to chat with api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { InputModule } from '@noah231515/receipt-wrangler-core';
+import { ApiModule, InputModule } from '@noah231515/receipt-wrangler-core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    ApiModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
+    HttpClientModule,
     InputModule,
+    IonicModule.forRoot(),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
