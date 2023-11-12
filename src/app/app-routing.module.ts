3d175d342e6a0102a1cc2821951a05ee1e5fb936
Noah <noah231515@gmail.com>
2023-11-12 16:41:10 -0500

    update packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadChildren: () =>
-  //     import('./tabs/tabs.module').then((m) => m.TabsPageModule),
-  // },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
+  },
   {
     path: 'auth',
     loadChildren: () =>
