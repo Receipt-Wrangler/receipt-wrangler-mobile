8a50daef5109dc48b9659d0bc41ee2784b8236ae
Noah <noah231515@gmail.com>
2024-01-11 08:32:22 -0500

    see groups list

--- src/app/groups/groups-list/groups-list.component.ts
+++ src/app/groups/groups-list/groups-list.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { Group, GroupState } from '@receipt-wrangler/receipt-wrangler-core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-groups-list',
   templateUrl: './groups-list.component.html',
   styleUrls: ['./groups-list.component.scss'],
 })
-export class GroupsListComponent  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class GroupsListComponent {
+  @Select(GroupState.groups) public groups!: Observable<Group[]>;
 }
--- src/app/groups/groups.module.ts
+++ src/app/groups/groups.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { GroupsRoutingModule } from './group-routing.module';
 import { GroupsListComponent } from './groups-list/groups-list.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [GroupsListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule],
   exports: [GroupsRoutingModule],
 })
 export class GroupsModule {}
