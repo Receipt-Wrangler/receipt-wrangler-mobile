523fbc0de6d968e0400ffe868a3a973dbe02510e
Noah <noah231515@gmail.com>
2024-01-12 08:02:00 -0500

    get extension working

--- src/app/auth/mobile-auth-form/mobile-auth-form.component.ts
+++ src/app/auth/mobile-auth-form/mobile-auth-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngxs/store';
 import {
   AppInitService,
   AuthForm,
   AuthFormUtil,
+  UserValidators,
 } from '@receipt-wrangler/receipt-wrangler-core';
 import { switchMap, take, tap } from 'rxjs';
 import { ServerState } from 'src/app/store/server.state';
@@ -14,21 +15,27 @@ import { ServerState } from 'src/app/store/server.state';
   selector: 'app-mobile-auth-form',
   templateUrl: './mobile-auth-form.component.html',
   styleUrls: ['./mobile-auth-form.component.scss'],
+  providers: [UserValidators],
 })
-export class MobileAuthFormComponent implements OnInit {
+export class MobileAuthFormComponent extends AuthForm implements OnInit {
   @ViewChild(AuthForm) public authForm!: AuthForm;
 
   public homeserverUrlFormControl!: FormControl;
 
   constructor(
     private appInitService: AppInitService,
-    private formBuilder: FormBuilder,
-    private store: Store,
-    private authFormUtil: AuthFormUtil,
-    private router: Router
-  ) {}
+    protected override authFormUtil: AuthFormUtil,
+    protected override formBuilder: FormBuilder,
+    protected override route: ActivatedRoute,
+    protected override router: Router,
+    protected override store: Store,
+    protected override userValidators: UserValidators
+  ) {
+    super(authFormUtil, formBuilder, route, router, store, userValidators);
+  }
 
-  public ngOnInit() {
+  public override ngOnInit() {
+    super.ngOnInit();
     this.initHomeserverUrlFormControl();
   }
 
@@ -38,7 +45,7 @@ export class MobileAuthFormComponent implements OnInit {
     );
   }
 
-  public submit(): void {
+  public override submit(): void {
     if (this.authForm.form.valid) {
       this.authFormUtil
         .getSubmitObservable(this.authForm.form, this.authForm.isSignUp.value)
