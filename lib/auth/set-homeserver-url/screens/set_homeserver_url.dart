dccf042d5c1797d3c4d1cc2d18dd00afb5c03ceb
Noah <noah231515@gmail.com>
2024-01-22 21:12:38 -0500

    more clean up

--- lib/auth/set-homeserver-url/screens/set_homeserver_url.dart
+++ lib/auth/set-homeserver-url/screens/set_homeserver_url.dart
@@ -18,19 +18,13 @@ class _SetHomeserverUrl extends State<SetHomeserverUrl> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Form(
       key: _formKey,
       child: Column(
         children: [
           const Text("Set Homeserver URL"),
           TextFormField(
-            decoration: const InputDecoration(label: Text("Homeserver URl")),
+            decoration: const InputDecoration(label: Text("Homeserver URL")),
             validator: (value) {
               if (value == null || value.isEmpty) {
                 return "Text is required";
