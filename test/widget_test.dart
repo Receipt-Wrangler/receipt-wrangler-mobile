c4777c9d71cc3f54da9c05bf2b77b5e78802bb85
Noah <noah231515@gmail.com>
2024-01-22 21:07:36 -0500

    bit of tutorial clean up

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:receipt_wrangler_mobile/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ReceiptWrangler());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
