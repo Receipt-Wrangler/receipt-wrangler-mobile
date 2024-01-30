022ebdc9410bfc1c5481957fb5ce82418b438324
Noah <noah231515@gmail.com>
2024-01-30 07:34:49 -0500

    wip

--- lib/guards/auth-guard.dart
+++ lib/guards/auth-guard.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:receipt_wrangler_mobile/api/api.dart';
 import 'package:receipt_wrangler_mobile/models/auth_model.dart';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 
 Future<String?> canNavigateTo(BuildContext context, String? redirect) async {
-  return Provider.of<AuthModel>(context, listen: false).getJwt().then((jwt) {
+  return Provider.of<AuthModel>(context, listen: false)
+      .getJwt()
+      .then((jwt) async {
     var defaultRoute = "/";
+    var refreshToken =
+        await Provider.of<AuthModel>(context, listen: false).getRefreshToken();
 
     if (isTokenValid(jwt)) {
       return redirect ?? defaultRoute;
     } else {
-      return defaultRoute;
+      if (isTokenValid(refreshToken)) {
+        var tokenPair = await AuthApi().getNewRefreshToken();
+      } else {
+        // purge old tokens
+        return defaultRoute;
+      }
     }
   });
 }
 
 bool isTokenValid(String? token) {
-  return token!.isNotEmpty;
+  if (token == null || token.isEmpty) {
+    return false;
+  } else {
+    var claims = JWT.decode(token);
+    DateTime expiration = DateTime.fromMillisecondsSinceEpoch(
+        claims.payload["exp"] * 1000,
+        isUtc: false);
+
+    return expiration.isAfter(DateTime.now());
+  }
 }
