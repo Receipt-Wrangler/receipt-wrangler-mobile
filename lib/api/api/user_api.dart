b05eb55e79e90a31d890f14293860ff7ae7191d9
Noah <noah231515@gmail.com>
2024-01-31 10:13:18 -0500

    update api

--- lib/api/api/user_api.dart
+++ lib/api/api/user_api.dart
@@ -237,6 +237,54 @@ class UserApi {
     }
   }
 
+  /// Get app data
+  ///
+  /// This will return the user's app data for the currently logged in user [SYSTEM USER]
+  ///
+  /// Note: This method returns the HTTP [Response].
+  Future<Response> getAppDataWithHttpInfo() async {
+    // ignore: prefer_const_declarations
+    final path = r'/user/appData';
+
+    // ignore: prefer_final_locals
+    Object? postBody;
+
+    final queryParams = <QueryParam>[];
+    final headerParams = <String, String>{};
+    final formParams = <String, String>{};
+
+    const contentTypes = <String>[];
+
+
+    return apiClient.invokeAPI(
+      path,
+      'GET',
+      queryParams,
+      postBody,
+      headerParams,
+      formParams,
+      contentTypes.isEmpty ? null : contentTypes.first,
+    );
+  }
+
+  /// Get app data
+  ///
+  /// This will return the user's app data for the currently logged in user [SYSTEM USER]
+  Future<AppData?> getAppData() async {
+    final response = await getAppDataWithHttpInfo();
+    if (response.statusCode >= HttpStatus.badRequest) {
+      throw ApiException(response.statusCode, await _decodeBodyBytes(response));
+    }
+    // When a remote server returns no body with a status of 204, we shall not decode it.
+    // At the time of writing this, `dart:convert` will throw an "Unexpected end of input"
+    // FormatException when trying to decode an empty string.
+    if (response.body.isNotEmpty && response.statusCode != HttpStatus.noContent) {
+      return await apiClient.deserializeAsync(await _decodeBodyBytes(response), 'AppData',) as AppData;
+    
+    }
+    return null;
+  }
+
   /// Get claims for logged in user
   ///
   /// This will return the user's token claims for the currently logged in user [SYSTEM USER]
