911cf21c17a1c304cc582a8b54e48ed757bfb4da
Noah <noah231515@gmail.com>
2024-02-07 22:27:42 -0500

    format text

--- lib/groups/widgets/dashboard_widgets/group_summary.dart
+++ lib/groups/widgets/dashboard_widgets/group_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:receipt_wrangler_mobile/api/api.dart' as api;
 import 'package:receipt_wrangler_mobile/models/user_model.dart';
@@ -14,36 +15,39 @@ class _GroupSummary extends State<GroupSummary> {
   String _getUserOwesText(
       MapEntry<String, String> mapEntry, UserModel userModel) {
     var user = userModel.getUserById(mapEntry.key);
+    var amount = double.parse(mapEntry.value);
+    var formattedAmount =
+        NumberFormat.currency(decimalDigits: 2, name: "\$").format(amount);
 
     if (mapEntry.value.contains("-") || mapEntry.value == "0") {
-      return "You owe ${user!.displayName}: ${mapEntry.value}";
+      return "You owe ${user!.displayName}: $formattedAmount";
     } else {
-      return "${user!.displayName} owes you: ${mapEntry.value}";
+      return "${user!.displayName} owes you: $formattedAmount";
     }
+  }
+
+  List<Widget> buildSummaryLineWidgets(Map<String, String>? userData) {
+    var nothingOwed = [const Text("Phew, you're all caught up!")];
+    var widgets = <Widget>[];
+    if (userData == null || userData.isEmpty) {
+      return nothingOwed;
+    }
+    var userModel = Provider.of<UserModel>(context, listen: false);
+
+    userData.entries.forEach((element) {
+      widgets.add(Row(
+        children: [
+          Text(_getUserOwesText(element, userModel)),
+        ],
+      ));
+    });
+
+    return widgets;
   }
 
   @override
   Widget build(BuildContext context) {
     var groupSummaryFuture = api.UserApi().getAmountOwedForUser(groupId: 1);
-
-    List<Widget> buildSummaryLineWidgets(Map<String, String>? userData) {
-      var nothingOwed = [const Text("Phew, you're all caught up!")];
-      var widgets = <Widget>[];
-      if (userData == null || userData.isEmpty) {
-        return nothingOwed;
-      }
-      var userModel = Provider.of<UserModel>(context, listen: false);
-
-      userData.entries.forEach((element) {
-        widgets.add(Row(
-          children: [
-            Text(_getUserOwesText(element, userModel)),
-          ],
-        ));
-      });
-
-      return widgets;
-    }
 
     return FutureBuilder(
         future: groupSummaryFuture,
