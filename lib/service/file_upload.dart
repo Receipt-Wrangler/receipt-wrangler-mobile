51431e8b06828c8ff3d932e5c900bcb14a102c5c
Noah <noah231515@gmail.com>
2024-03-09 21:55:26 -0500

    todo

--- lib/service/file_upload.dart
+++ lib/service/file_upload.dart
@@ -1,14 +1,16 @@
 import 'dart:io';
+
 import 'package:http/http.dart';
 import 'package:receipt_wrangler_mobile/api/api.dart';
 import 'package:receipt_wrangler_mobile/utils/scan.dart';
 
 Future<int> uploadImagesToReceipt(String receiptId) async {
   try {
+    // TODO: pass back the file data views, then add them to receipt via receipt provider. We can use an images array stream, then set up a listener in the carousel
     var filePaths = await scanImages(5) ?? [];
     for (var filePath in filePaths) {
       var multipartFile = await MultipartFile.fromPath("file", filePath);
-      await ReceiptImageApi()
+      var fileDataView = await ReceiptImageApi()
           .uploadReceiptImage(multipartFile, int.parse(receiptId));
       await File(filePath).delete();
     }
