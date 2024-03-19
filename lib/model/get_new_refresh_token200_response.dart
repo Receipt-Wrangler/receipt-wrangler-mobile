0a9cdac883bcc0978024dffcbbf1422ff31a74ab
Noah2315 <Noah231515>
2024-03-19 16:47:16 -0700

    fix 200

--- lib/model/get_new_refresh_token200_response.dart
+++ lib/model/get_new_refresh_token200_response.dart
@@ -12,22 +12,8 @@ part of openapi.api;
 
 class GetNewRefreshToken200Response {
   /// Returns a new [GetNewRefreshToken200Response] instance.
-  GetNewRefreshToken200Response({
-    required this.jwt,
-    required this.refreshToken,
-    this.userId = 0,
-    required this.userRole,
-    this.displayName = '',
-    this.defaultAvatarColor = '',
-    this.username = '',
-    this.iss = '',
-    this.sub = '',
-    this.aud = const [],
-    this.exp = 0,
-    this.nbf = 0,
-    this.iat = 0,
-    this.jti = '',
-  });
+  GetNewRefreshToken200Response(
+      {required this.jwt, required this.refreshToken});
 
   /// JWT token
   String jwt;
@@ -35,95 +21,26 @@ class GetNewRefreshToken200Response {
   /// Refresh token
   String refreshToken;
 
-  /// User foreign key
-  int userId;
-
-  UserRole userRole;
-
-  /// Display name
-  String displayName;
-
-  /// Default avatar color
-  String defaultAvatarColor;
-
-  /// User's username used to login
-  String username;
-
-  /// Issuer
-  String iss;
-
-  /// Subject
-  String sub;
-
-  /// Audience
-  List<String> aud;
-
-  /// Expiration time
-  int exp;
-
-  /// Not before
-  int nbf;
-
-  /// Issued at
-  int iat;
-
-  /// JWT ID
-  String jti;
-
   @override
-  bool operator ==(Object other) => identical(this, other) || other is GetNewRefreshToken200Response &&
-     other.jwt == jwt &&
-     other.refreshToken == refreshToken &&
-     other.userId == userId &&
-     other.userRole == userRole &&
-     other.displayName == displayName &&
-     other.defaultAvatarColor == defaultAvatarColor &&
-     other.username == username &&
-     other.iss == iss &&
-     other.sub == sub &&
-     other.aud == aud &&
-     other.exp == exp &&
-     other.nbf == nbf &&
-     other.iat == iat &&
-     other.jti == jti;
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is GetNewRefreshToken200Response &&
+          other.jwt == jwt &&
+          other.refreshToken == refreshToken;
 
   @override
   int get hashCode =>
-    // ignore: unnecessary_parenthesis
-    (jwt.hashCode) +
-    (refreshToken.hashCode) +
-    (userId.hashCode) +
-    (userRole.hashCode) +
-    (displayName.hashCode) +
-    (defaultAvatarColor.hashCode) +
-    (username.hashCode) +
-    (iss.hashCode) +
-    (sub.hashCode) +
-    (aud.hashCode) +
-    (exp.hashCode) +
-    (nbf.hashCode) +
-    (iat.hashCode) +
-    (jti.hashCode);
+      // ignore: unnecessary_parenthesis
+      (jwt.hashCode) + (refreshToken.hashCode);
 
   @override
-  String toString() => 'GetNewRefreshToken200Response[jwt=$jwt, refreshToken=$refreshToken, userId=$userId, userRole=$userRole, displayName=$displayName, defaultAvatarColor=$defaultAvatarColor, username=$username, iss=$iss, sub=$sub, aud=$aud, exp=$exp, nbf=$nbf, iat=$iat, jti=$jti]';
+  String toString() =>
+      'GetNewRefreshToken200Response[jwt=$jwt, refreshToken=$refreshToken]';
 
   Map<String, dynamic> toJson() {
     final json = <String, dynamic>{};
-      json[r'jwt'] = this.jwt;
-      json[r'refreshToken'] = this.refreshToken;
-      json[r'userId'] = this.userId;
-      json[r'userRole'] = this.userRole;
-      json[r'displayName'] = this.displayName;
-      json[r'defaultAvatarColor'] = this.defaultAvatarColor;
-      json[r'username'] = this.username;
-      json[r'iss'] = this.iss;
-      json[r'sub'] = this.sub;
-      json[r'aud'] = this.aud;
-      json[r'exp'] = this.exp;
-      json[r'nbf'] = this.nbf;
-      json[r'iat'] = this.iat;
-      json[r'jti'] = this.jti;
+    json[r'jwt'] = this.jwt;
+    json[r'refreshToken'] = this.refreshToken;
     return json;
   }
 
@@ -139,35 +56,25 @@ class GetNewRefreshToken200Response {
       // Note 2: this code is stripped in release mode!
       assert(() {
         requiredKeys.forEach((key) {
-          assert(json.containsKey(key), 'Required key "GetNewRefreshToken200Response[$key]" is missing from JSON.');
-          assert(json[key] != null, 'Required key "GetNewRefreshToken200Response[$key]" has a null value in JSON.');
+          assert(json.containsKey(key),
+              'Required key "GetNewRefreshToken200Response[$key]" is missing from JSON.');
+          assert(json[key] != null,
+              'Required key "GetNewRefreshToken200Response[$key]" has a null value in JSON.');
         });
         return true;
       }());
 
       return GetNewRefreshToken200Response(
-        jwt: mapValueOfType<String>(json, r'jwt')!,
-        refreshToken: mapValueOfType<String>(json, r'refreshToken')!,
-        userId: mapValueOfType<int>(json, r'userId')!,
-        userRole: UserRole.fromJson(json[r'userRole'])!,
-        displayName: mapValueOfType<String>(json, r'displayName')!,
-        defaultAvatarColor: mapValueOfType<String>(json, r'defaultAvatarColor')!,
-        username: mapValueOfType<String>(json, r'username')!,
-        iss: mapValueOfType<String>(json, r'iss')!,
-        sub: mapValueOfType<String>(json, r'sub') ?? '',
-        aud: json[r'aud'] is List
-            ? (json[r'aud'] as List).cast<String>()
-            : const [],
-        exp: mapValueOfType<int>(json, r'exp')!,
-        nbf: mapValueOfType<int>(json, r'nbf') ?? 0,
-        iat: mapValueOfType<int>(json, r'iat') ?? 0,
-        jti: mapValueOfType<String>(json, r'jti') ?? '',
-      );
+          jwt: mapValueOfType<String>(json, r'jwt')!,
+          refreshToken: mapValueOfType<String>(json, r'refreshToken')!);
     }
     return null;
   }
 
-  static List<GetNewRefreshToken200Response>? listFromJson(dynamic json, {bool growable = false,}) {
+  static List<GetNewRefreshToken200Response>? listFromJson(
+    dynamic json, {
+    bool growable = false,
+  }) {
     final result = <GetNewRefreshToken200Response>[];
     if (json is List && json.isNotEmpty) {
       for (final row in json) {
@@ -195,12 +102,18 @@ class GetNewRefreshToken200Response {
   }
 
   // maps a json object with a list of GetNewRefreshToken200Response-objects as value to a dart map
-  static Map<String, List<GetNewRefreshToken200Response>> mapListFromJson(dynamic json, {bool growable = false,}) {
+  static Map<String, List<GetNewRefreshToken200Response>> mapListFromJson(
+    dynamic json, {
+    bool growable = false,
+  }) {
     final map = <String, List<GetNewRefreshToken200Response>>{};
     if (json is Map && json.isNotEmpty) {
       json = json.cast<String, dynamic>(); // ignore: parameter_assignments
       for (final entry in json.entries) {
-        final value = GetNewRefreshToken200Response.listFromJson(entry.value, growable: growable,);
+        final value = GetNewRefreshToken200Response.listFromJson(
+          entry.value,
+          growable: growable,
+        );
         if (value != null) {
           map[entry.key] = value;
         }
@@ -222,4 +135,3 @@ class GetNewRefreshToken200Response {
     'exp',
   };
 }
-
