b474bf3c037b6db741e69ea8aff76c298ceb0bc6
Noah <navi0001@protonmail.com>
2024-04-20 08:57:07 -0400

    remove stream

--- lib/models/receipt_model.dart
+++ lib/models/receipt_model.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:receipt_wrangler_mobile/api.dart';
 import 'package:receipt_wrangler_mobile/utils/receipts.dart';
@@ -9,20 +7,11 @@ class ReceiptModel extends ChangeNotifier {
 
   Receipt get receipt => _receipt;
 
-  final StreamController<FileDataView> _streamController =
-      StreamController<FileDataView>.broadcast();
-
-  Stream<FileDataView> get imagesStream => _streamController.stream;
-
   void setReceipt(Receipt receipt, bool notify) {
     _receipt = receipt;
 
     if (notify) {
       notifyListeners();
     }
-  }
-
-  void pushImage(FileDataView file) {
-    _streamController.add(file);
   }
 }
