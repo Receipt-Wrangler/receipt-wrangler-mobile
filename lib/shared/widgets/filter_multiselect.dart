2660b11055fc868bafb89c78d99648775641719f
Noah <navi0001@protonmail.com>
2024-06-20 08:34:10 -0400

    move stuff out of built

--- lib/shared/widgets/filter_multiselect.dart
+++ lib/shared/widgets/filter_multiselect.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class FilterMultiSelect<T> extends StatefulWidget {
@@ -41,44 +42,64 @@ class FilterMultiSelectState<T> extends State<FilterMultiSelect<T>> {
     super.dispose();
   }
 
+  Widget buildFilterBar() {
+    return FormBuilderTextField(
+      name: "filter",
+      decoration: const InputDecoration(labelText: "Filter"),
+      onChanged: (String? value) {
+        setState(() {
+          print("fired");
+          filteredOptions = widget.options
+              .where((element) => widget
+                  .itemDisplayName(element)
+                  .toLowerCase()
+                  .contains(value!.toLowerCase()))
+              .toList();
+        });
+      },
+    );
+  }
+
+  Widget buildChoiceChip(T option, int index) {
+    return ChoiceChip(
+      label: Text(widget.itemDisplayName(option)),
+      selected: selectedOptions.contains(option),
+      onSelected: (bool selected) {
+        setState(() {
+          if (selected) {
+            selectedOptions.add(option);
+          } else {
+            selectedOptions.remove(option);
+          }
+        });
+      },
+    );
+  }
+
+  Widget buildChoiceChipGrid() {
+    return MasonryGridView.count(
+      shrinkWrap: true,
+      itemCount: filteredOptions.length,
+      itemBuilder: (BuildContext context, int index) {
+        return buildChoiceChip(widget.options[index], index);
+      },
+      crossAxisCount: 3,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    Widget buildFilterBar() {
-      return Text("placeholder");
-    }
+    print("built");
 
-    Widget buildChoiceChip(T option, int index) {
-      return ChoiceChip(
-        label: Text(widget.itemDisplayName(option)),
-        selected: selectedOptions.contains(option),
-        onSelected: (bool selected) {
-          setState(() {
-            if (selected) {
-              selectedOptions.add(option);
-            } else {
-              selectedOptions.remove(option);
-            }
-          });
-        },
-      );
-    }
-
-    Widget buildChoiceChipGrid() {
-      return MasonryGridView.count(
-        shrinkWrap: true,
-        itemCount: filteredOptions.length,
-        itemBuilder: (BuildContext context, int index) {
-          return buildChoiceChip(widget.options[index], index);
-        },
-        crossAxisCount: 3,
-      );
-    }
-
-    return Column(
-      children: [
-        buildFilterBar(),
-        buildChoiceChipGrid(),
-      ],
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          buildFilterBar(),
+          SizedBox(height: 10),
+          buildChoiceChipGrid(),
+        ],
+      ),
     );
   }
 }
