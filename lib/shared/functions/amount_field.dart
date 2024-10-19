d7bdb0b7a7c86dbf900bcabe99e7e08ead14358c
Noah <navi0001@protonmail.com>
2024-10-19 09:25:53 -0400

    initial implementation

--- lib/shared/functions/amount_field.dart
+++ lib/shared/functions/amount_field.dart
@@ -1,10 +1,11 @@
-import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:currency_textfield/currency_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
-import 'package:intl/intl.dart';
+import 'package:openapi/openapi.dart' as api;
 import 'package:provider/provider.dart';
 import 'package:receipt_wrangler_mobile/enums/form_state.dart';
+import 'package:receipt_wrangler_mobile/utils/currency.dart';
 
 import '../../models/system_settings_model.dart';
 import '../../utils/forms.dart';
@@ -13,23 +14,51 @@ Widget amountField(BuildContext context, String label, String fieldName,
     String initialAmount, WranglerFormState formState,
     {Key? key}) {
   var systemSettingsModel = Provider.of<SystemSettingsModel>(context);
+  var hideDecimalPlaces = systemSettingsModel.currencyHideDecimalPlaces;
+  var doubleAmount = double.parse(initialAmount);
 
-  var amountMask = new CurrencyTextInputFormatter(
-      NumberFormat.currency(customPattern: '###.###,##'),
-      enableNegative: false);
+  var controller = CurrencyTextFieldController(
+      decimalSymbol: hideDecimalPlaces
+          ? ""
+          : getCurrencySeparatorLiteral(
+              systemSettingsModel.currencyDecimalSeparator),
+      thousandSymbol: getCurrencySeparatorLiteral(
+          systemSettingsModel.currencyThousandSeparator),
+      initDoubleValue: doubleAmount,
+      numberOfDecimals: hideDecimalPlaces ? 0 : 2,
+      currencySymbol: "",
+      startWithSeparator: true,
+      forceCursorToEnd: false);
+
+  var prefix = systemSettingsModel.currencySymbolPosition ==
+          api.CurrencySymbolPosition.START
+      ? systemSettingsModel.currencyDisplay
+      : null;
+
+  var suffix = systemSettingsModel.currencySymbolPosition ==
+          api.CurrencySymbolPosition.END
+      ? systemSettingsModel.currencyDisplay
+      : null;
+
   return FormBuilderTextField(
     key: key,
     name: fieldName,
     decoration: InputDecoration(
-        labelText: label, prefixText: systemSettingsModel.currencyDisplay),
+      labelText: label,
+      prefixText: prefix,
+      suffixText: suffix,
+    ),
     keyboardType: TextInputType.number,
-    initialValue: initialAmount,
     validator: FormBuilderValidators.compose([
       FormBuilderValidators.required(),
-      FormBuilderValidators.numeric(),
-      FormBuilderValidators.min(0, inclusive: false),
+      //FormBuilderValidators.numeric(),
+      //FormBuilderValidators.min(0, inclusive: false),
     ]),
     readOnly: isFieldReadOnly(formState),
-    inputFormatters: [amountMask],
+    controller: controller,
+    valueTransformer: (value) {
+      // TODO: fix euros
+      return value?.trim();
+    },
   );
 }
--- lib/utils/currency.dart
+++ lib/utils/currency.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
+import 'package:openapi/openapi.dart';
 import 'package:provider/provider.dart';
 
 import '../models/system_settings_model.dart';
@@ -9,4 +10,15 @@ String formatCurrency(BuildContext context, double amount) {
   return NumberFormat.currency(
           decimalDigits: 2, name: systemSettingsModel.currencyDisplay)
       .format(amount);
+}
+
+String getCurrencySeparatorLiteral(CurrencySeparator separator) {
+  switch (separator) {
+    case CurrencySeparator.comma:
+      return ',';
+    case CurrencySeparator.period:
+      return '.';
+    default:
+      return '';
+  }
 }
