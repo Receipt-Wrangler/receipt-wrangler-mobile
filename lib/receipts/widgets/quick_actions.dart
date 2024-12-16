4548eebad3a4a2211bd60991bfafc3e2b96285be
Noah <navi0001@protonmail.com>
2024-12-16 10:19:09 -0500

    Get toggle set up

--- lib/receipts/widgets/quick_actions.dart
+++ lib/receipts/widgets/quick_actions.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:openapi/openapi.dart' as api;
+import 'package:provider/provider.dart';
+import 'package:receipt_wrangler_mobile/models/user_model.dart';
+
+import '../../models/context_model.dart';
+import '../../shared/functions/multi_select_bottom_sheet.dart';
+import '../../shared/widgets/multi-select-field.dart';
 
 class ReceiptQuickActions extends StatefulWidget {
   const ReceiptQuickActions({super.key});
@@ -8,30 +16,64 @@ class ReceiptQuickActions extends StatefulWidget {
 }
 
 class _ReceiptQuickActions extends State<ReceiptQuickActions> {
+  late final shellContext =
+      Provider.of<ContextModel>(context, listen: false).shellContext;
+  late final users = Provider.of<UserModel>(context, listen: false).users;
   var quickActions = ["Split Evenly", "Split Evenly with Portions"];
   var quickActionsSelection = [true, false];
+  final formKey = GlobalKey<FormBuilderState>();
+
+  // TODO: Get users in group
+  Widget buildUserField() {
+    return MultiSelectField<api.UserView>(
+        name: "userIds",
+        label: "Users",
+        initialValue: [],
+        itemDisplayName: (user) => user.displayName ?? "",
+        itemName: "Users",
+        onTap: showUserMultiSelect);
+  }
+
+  void showUserMultiSelect() {
+    showMultiselectBottomSheet(shellContext, "Select Users", "Select", users,
+        [], (user) => user.displayName).then((value) {
+      if (value != null) {
+        var users =
+            List<api.UserView>.from(value.map((item) => item as api.UserView));
+
+        setState(() {
+          formKey.currentState!.fields["userIds"]!.setValue(users);
+        });
+      }
+    });
+  }
+
+  Widget buildToggleButtons() {
+    return ToggleButtons(
+      children: [...quickActions.map((action) => Text(action))],
+      isSelected: quickActionsSelection,
+      onPressed: (selected) => setState(() {
+        var newState = !quickActionsSelection[selected];
+
+        quickActionsSelection = [false, false];
+        quickActionsSelection[selected] = newState;
+      }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+    return FormBuilder(
+        key: formKey,
+        child: Column(
           children: [
-            ToggleButtons(
-              children: [...quickActions.map((action) => Text(action))],
-              isSelected: quickActionsSelection,
-              onPressed: (selected) => setState(() {
-                var newState = !quickActionsSelection[selected];
-
-                quickActionsSelection = [false, false];
-                quickActionsSelection[selected] = newState;
-              }),
-            )
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [buildToggleButtons()],
+            ),
+            buildUserField(),
           ],
-        )
-      ],
-    );
+        ));
   }
 }
