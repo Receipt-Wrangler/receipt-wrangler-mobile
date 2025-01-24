f21dc4d6fcada182c6d775013c840a91e46387b1
Noah <navi0001@protonmail.com>
2025-01-24 10:23:37 -0500

    lead

--- lib/groups/widgets/group_activity_list_item.dart
+++ lib/groups/widgets/group_activity_list_item.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:openapi/openapi.dart' as api;
 import 'package:provider/provider.dart';
 import 'package:receipt_wrangler_mobile/shared/functions/activities.dart';
-import 'package:receipt_wrangler_mobile/shared/widgets/date_block.dart';
 import 'package:receipt_wrangler_mobile/shared/widgets/slidable_widget.dart';
 
+import '../../constants/colors.dart';
 import '../../models/auth_model.dart';
 import '../../models/group_model.dart';
 import '../../shared/functions/permissions.dart';
+import '../../shared/widgets/list_item_lead.dart';
 
 class GroupActivityListItem extends StatefulWidget {
   const GroupActivityListItem(
@@ -25,9 +26,27 @@ class _GroupActivityListItem extends State<GroupActivityListItem> {
   late final authModel = Provider.of<AuthModel>(context, listen: false);
   late final groupModel = Provider.of<GroupModel>(context, listen: false);
 
+  Color getActivityColor() {
+    switch (widget.activity.status) {
+      case api.SystemTaskStatus.SUCCEEDED:
+        return successGreen;
+      case api.SystemTaskStatus.FAILED:
+        return errorRed;
+      default:
+        return Colors.grey;
+    }
+  }
+
+  Widget getLeadingWidget() {
+    return ListItemLead(
+      date: widget.activity.startedAt,
+      color: getActivityColor(),
+    );
+  }
+
   Widget buildListTile() {
     return ListTile(
-      leading: DateBlock(date: widget.activity.startedAt),
+      leading: getLeadingWidget(),
       title: Text(getActivityTypeDisplay(widget.activity.type)),
       trailing: Text(getActivityStatusDisplay(widget.activity.status)),
     );
